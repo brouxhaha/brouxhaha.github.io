20b19273e59d98b7ef262f7fa7129265d3c436ce
brouxhaha <andy.debroux@gmail.com>
2018-07-08 18:31:44 -0700

    updating to vue.js website

---
+++ src/components/About.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="panel panel--odd">
+            <h2 class="panel__heading">Career</h2>
+            <div class="wrapper">
+                <div class="content">
+                    <h3> I am a former tax accountant</h3>
+                    <p>That's right, my first degree was a double major in accounting and finance. I had an internship in Kimberly-Clark's tax department for about a year and 9 months, and somehow I didn't realize I really didn't want to do accounting until the month before graduation. Of course, as an accounting major, I was in year 5 since 150 credits are required to sit for the CPA exam. I figured I may as well give it a chance and, in the meantime, figure out what I actually wanted to do. </p>
+                    <h3>Problem Solving: Good | Accounting: Bad</h3>
+                    <p>Maybe 2.5 years later, my wife suggested I look into graphic design. I researched everything I could about it, read forums, and decided it sounded like a good move. I enrolled part-time at Drake University to see if it was something I would enjoy while I worked full-time. My first two classes were 3D Design and an Adobe software class (Illustrator, Photoshop, and InDesign—I can't recall the actual name of the class). I loved it, and it actually made work more enjoyable even though I was working about 45 hours a week, sitting in class 9 hours a week, and working on projects on the weekends.</p>
+                    <p>I quit my accounting job that August, and I continued mostly part-time to finish my graphic design degree over the next two years. I started my original college degree majoring in computer science, so I thought the web side of things, where I could also code, would interest me more. Luckily, my first job was a dual role as a web/graphic designer. </p>
+                    <h3>Hello Seattle</h3>
+                    <p>My wife's contract with the USDA was ending, so we had the opportunity to leave Iowa (where we had moved so she could attend law school and we had no family). We chose Seattle for our want to live in a bigger city while having access to nature and it's progressiveness. We moved to Seattle about four months after making that decision, and we haven't looked back. </p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel--even">
+            <h2 class="panel__heading">Personal</h2>
+            <div class="wrapper">
+                <div class="content">
+                    <h3>A bit about me</h3>
+                    <p>My family is my number one priority, and I spend as much time as I can with my wife and almost 4-year-old daughter. My main hobbies these days involve entertaining and teaching my daughter. I am often, slowly but surely, working on my side projects. I try to spend an hour or more on them a day, but there are days where that just doesn't happen.</p>
+                    <p>Outside of my family and development and design, I enjoy improving our yard and working on home improvement projects. So far, I've replaced our main floor's carpet with bamboo and replaced our front stairs. I also like to read and try to read a few hours a week. Drawing and illustration is something I barely ever have time to do, but every now and then I find time to work on something.</p>
+                    <p>Being active is highly important to me. I used to run, but I've had surgery on both hips to fix an impingement issue (femoral acetabular impingement), so running is no longer a good option for me. Luckily, I am currently able to commute by bike about 9.5 miles each day. I really enjoy being in nature and going for walks and hikes.</p>
+                    <p>Our dog, Cairo, is 115 pounds, so I get a full-body workout every day when I walk him. We also have two cats, Beowulf and Zeus.</p>
+                    <p>Oh, and I love coffee and beer.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/MainPanels/MainPanelAbout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="home__about">
+        <div class="home__greeting intro">
+            <h1 class="centered">Andy's Life Story</h1>
+            <p>No, not really. More of a backstory and how I came to be where I am. I appreciate you taking an interest.</p>
+            
+        </div>
+        <div class="home__portrait">
+            <img src="/src/assets/portrait-fade.png" alt="portrait of Andy" class="portrait__img">
+        </div>
+    </div>
+</template>
